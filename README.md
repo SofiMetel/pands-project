04f7b215c1434d0558a3660e9c202298ec51c134
SofiMetel <60064845+SofiMetel@users.noreply.github.com>
2023-05-12 22:05:11 +0100

    first part of last commit
    first part of documentation

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# pands-project
- project for Programming and Scripting 
+### pands-project
+### project for Programming and Scripting 
 
-The data set consists of 50 samples from each of three species of Iris (Iris setosa, Iris virginica and Iris versicolor). Four features were measured from each sample: the length and the width of the sepals and petals, in centimeters. Based on the combination of these four features, Fisher developed a linear discriminant model to distinguish the species from each other. Fisher's paper was published in the Annals of Eugenics and includes discussion of the contained techniques' applications to the field of phrenology.[1] https://en.wikipedia.org/wiki/Iris_flower_data_set#cite_note-5
+# Table of Contents
+<a name="-table-of-contents"></a>
 
-You might do that for this project as follows:
+1. [Introduction to the project](#1.-introduction-to-the-project) 
+2. [The Fisher Iris Data Set](#2.-the-fisher-iris-dataset) 
+3. [Downloading the Fisher Iris dataset](#3.-download-the-fisher-iris-dataset)
+4. [Program analysis.py](#4.-program-analysis-python)
+
+    4.1. [Output of summary of each variable to analysis.txt](#4.1.-output-of-summary-of-each-variable)
+
+    4.2.[Histograms of each variable as .png](#4.2.-histogram-of-each-variable-as-png)
+
+    4.3.[Scatter plots of each pair of variable](#4.3.-scatter-plots-of-each-variable)
+
+5. [Additional analysis](#5.-additional-analysis)
+6. [Summary and Conclusions](#6.-summary-and-conclusions)
+7. [References](#7.-references)
+
+
+
+# 1. Introduction to the project
+<a name="1.-introduction-to-the-project"></a>
+
+This project was made during Programming and Scripting module (Higher Diploma in Computing in Data Analytics) at ATU Galway. 
+
+Task that was given: 
+
+"You might do that for this project as follows:
 1. Research the data set online and write a summary about it in your README.
 2. Download the data set and add it to your repository.
 3. Write a program called analysis.py that:
@@ -12,7 +37,18 @@ You might do that for this project as follows:
     3. Outputs a scatter plot of each pair of variables.
     4. Performs any other analysis you think is appropriate
 
-# Pandas Iris Project
+
+Also, It might help to suppose that your manager has asked you to investigate the data set, with a
+view to explaining it to your colleagues. Imagine that you are to give a presentation on the
+data set in a few weeks’ time, where you explain what investigating a data set entails and how
+Python can be used to do it. You have not been asked to create a deck of presentation slides,
+but rather to present your code and its output to them.
+
+
+# 2. The Fisher Iris Data Set Research Online
+<a name="2.-the-fisher-iris-dataset"></a>
+
+The data set consists of 50 samples from each of three species of Iris (Iris setosa, Iris virginica and Iris versicolor). Four features were measured from each sample: the length and the width of the sepals and petals, in centimeters. Based on the combination of these four features, Fisher developed a linear discriminant model to distinguish the species from each other. Fisher's paper was published in the Annals of Eugenics and includes discussion of the contained techniques' applications to the field of phrenology.[1] https://en.wikipedia.org/wiki/Iris_flower_data_set#cite_note-5
 
 The Iris dataset is a popular dataset used in machine learning and data science. It consists of 150 observations of iris flowers, with 50 observations for each of the three species - Setosa, Versicolor, and Virginica. The dataset includes four features - sepal length, sepal width, petal length, and petal width.
 
@@ -24,7 +60,7 @@ The Pandas Iris project is a great way to learn and practice data manipulation, 
 
 In conclusion, the Pandas Iris project is a great way to learn and practice data science skills using a popular dataset. By using Pandas, we can easily manipulate and analyze the data, helping us gain insights and make data-driven decisions.
 
-'''
+
 Attribute Information:
 
 1. sepal length in cm
@@ -35,4 +71,101 @@ Attribute Information:
 -- Iris Setosa
 -- Iris Versicolour
 -- Iris Virginica
-'''
+
+
+
+
+# 3. Downloading the Fisher Iris dataset <a name="3.-download-the-fisher-iris-dataset"></a>
+
+I used Python programming language and Pands package that is built on top of it. 
+
+Dataset was downloaded from http://archive.ics.uci.edu/ml/datasets/Iris 
+
+On the top of analysis.py I added all packages that I will work with. 
+ 
+- [Pandas.pydata documentation](https://pandas.pydata.org/pandas-docs/stable/getting_started/index.html) 
+- [Matplotlib documentation](https://matplotlib.org/index.html) 
+- [Nympy documentation](https://numpy.org/doc/)
+
+
+I used Pandas module to read dataset in csv format. 
+Data is stored in csv (comma-separated values) file and when we read it with Pandas we create Pandas DataFrame. 
+
+DataFrame is amazing for data analysis of real world date because we can store all types of data and do calculation on columns and lines. 
+
+`read_csv` - Read a comma-separated values (csv) file into DataFrame.
+
+`
+col_names = ['sepal_length','sepal_width','petal_length','petal_width','class']
+iris =pd.read_csv('iris.csv', header=None, names=col_names)
+`
+
+
+
+# 4. Program analysis.py
+<a name="4.-program-analysis-python"></a>
+
+
+# 4.1. Output of summary of each variable to analysis.txt
+<a name="4.1.-output-of-summary-of-each-variable"></a>
+
+One of many ways to output summary of each variable is pivot_tables. 
+The "values" argument is the column that we want to summarize, and the index column is the column that you want to group by. By default, pivot_table takes the mean value for each group. 
+
+
+One of the most popular to output statistical summary of dataset is describe() 
+The pandas describe() function shows the following:
+
+The `count` or number of the non-NA/null observations in the dataset.
+
+The `max` shows the maximum of the values and the min shows the minimum of the values.
+
+The `mean` shows the mean or average of the values (the sum of data values divided by total number of values).
+The `std` shows the standard deviation  of the observations.
+
+The describe function also shows the 25th, 50th and 75th percentiles. 
+The 25th percentile shows the percentage of values falling below that percentile. 
+The 50th percentile shows the same information as the median would, that is where 50% of the values fall above and 50% fall below the value.
+
+
+Then after computing summary statistics over each class of Iris I create txt file and write all results into it
+```
+FILENAME = "analysis.txt"
+with open(FILENAME, 'wt') as f:
+    f.write("IRIS DATASET ANALYSIS \n")
+
+```
+
+in file analysis.txt next information can be found:
+    
+
+    
+    - First five lines of dataset
+    - Last five lines of dataset are
+    - unique values
+    - data types in Iris DataFrame
+    - Summary of data 
+    - Pivot table analysis of each class of iris
+    - The number of observations for each variable for each Iris species in the data set
+    - The mean or average measurement for each group of Iris Species in the dataset
+    - The maximum value for each measurement for each Class of Iris plant in the Iris dataset
+    - The minimum value for each measurement for each Class of Iris plant in the Iris dataset 
+    -Summary of each variable of each class
+
+
+
+
+# 4.2. Histograms of each variable as .png 
+<a name="4.2.-histogram-of-each-variable-as-png"></a>
+
+# 4.3. Scatter plots of each pair of variable
+<a name="4.3.-scatter-plots-of-each-variable"></a>
+
+# 5. Additional analysis<a name="#5.-additional-analysis"></a>
+# 6. Summary and Conclusions<a name="#6.-summary-and-conclusions"></a>
+# 7. References <a name="#7.-references"></a>
+
+- Website: [Python 3 documentation](https://docs.python.org/3/index.html) Official Python 3 Documentation at <https://docs.python.org>
+- Website: [Getting Started with pandas](https://pandas.pydata.org/pandas-docs/stable/getting_started/index.html) Official `pandas` tutorial  at <https://pandas.pydata.org>
+- Website: [DataCamp Data Manipulation with Pands](https://app.datacamp.com/learn/courses/data-manipulation-with-pandas) learning course at <https://app.datacamp.com/learn/courses/data-manipulation-with-pandas>
+-
--- analysis.py
+++ analysis.py
@@ -14,26 +14,124 @@ Attribute Information:
 -- Iris Versicolour
 -- Iris Virginica
 '''
+
+#read csv data into pandas DataFrame with column names as col_names
 col_names = ['sepal_length','sepal_width','petal_length','petal_width','class']
 
 iris =pd.read_csv('iris.csv', header=None, names=col_names)
+#show first 5 rows of dataFrame
 print(iris.head())
 
-print(iris.info())
-print('.info() shows information on each of the columns, such as the data type and number of missing values. There is no missing values in the data set')
 
+##############Part 4.1 ############
+#.info shows information on each of the columns, such as the data type and number of missing values. There is no missing values in the data set
+print(iris.info)
+
+datatypes = iris.dtypes
+#describe() prints descriptive statistics of data set
 print(iris.describe())
-print(iris.dtypes)
-print(iris['class'].unique())
+
+#unique() method on the 'Class' column to show how many different class or species of Iris flower is in the data set.
+unique_iris =iris['class'].unique()
+print(unique_iris)
 
 print(f"First five lines of dataset are {iris.head()} ")
 print(f"Last five lines of dataset are {iris.tail()} ")
 print(f'Summary of data :')
 print(iris.describe())
 
-# I took  Pivot tables idea from DataCamp course #4
-print(iris.pivot_table(index='class', values=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'], aggfunc=np.mean))
+
+
+'''I assigned code that do analysis to variables because
+ when I will do the output to .txt program will not repeat calculation
+ but just write the result to txt
+ Reference for pivot_table and group by: Data Camp Data Manupulating with Pandas
+ '''
+
+pivot_table = iris.pivot_table(index='class', 
+                               values=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'], aggfunc=np.mean)
+print(pivot_table)
 print(list(iris.columns.values))
+
+'''We can summary each var with pivot table using different numpy fuction but we also can use group by'''
+
+
+print("Using pands groupby function to split the iris dataframe by Class of iris species \n")
+# Using groupby functions to look at statistics at the class / species level
+iris_grouped = iris.groupby("class")
+
+# compute count of group, excluding missing values
+countbyclass = iris.groupby("class").count()
+
+
+# Groupby class of Iris plant and return the mean of the remaining columns in each group.
+
+meanbyclass = iris.groupby('class').mean()
+
+# get max of group values
+maxbyclass= iris.groupby("class").max()
+
+
+# get min of group values
+minbyclass=iris.groupby("class").min()
+
+#get summary statistics for each class of iris plant in the dataset can be obtained using the describe method on the GroupByobject.
+describebyclass = iris.groupby("class").describe()
+print(describebyclass)
+
+
+#########SAVE to txt file############### 
+FILENAME = "analysis.txt"
+with open(FILENAME, 'wt') as f:
+    f.write("IRIS DATASET ANALYSIS \n")
+
+    
+    f.write("First five lines of dataset are:   \n")
+    f.write(str(iris.head()))
+
+
+    f.write("Last five lines of dataset are:   \n")
+    f.write(str(iris.tail()))
+
+    f.write(f'Show all unique values : \n')
+    f.write(str(unique_iris))
+    f.write("   \n")
+    f.write(f'Data types in Iris DataFrame is : \n')
+    f.write(str(datatypes))
+
+
+    f.write("   \n")
+    f.write('Summary of data :  \n')
+    f.write("   \n")
+    f.write(str(iris.describe()))
+    f.write('\n   \n')
+
+    f.write(" Pivot table analysis of each class of iris:  \n")
+    f.write(str(pivot_table))
+    f.write('\n   \n')
+    f.write("The number of observations for each variable for each Iris species in the data set are as follows: \n")
+    f.write(str(countbyclass))
+    f.write('\n   \n')
+    f.write("The mean or average measurement for each group of Iris Species in the dataset is: \n")
+    f.write(str(meanbyclass))
+    f.write('\n   \n')
+    f.write("The maximum value for each measurement for each Class of Iris plant in the Iris dataset are:  \n")
+    f.write(str(maxbyclass))
+    f.write('\n   \n')
+    f.write("The minimum value for each measurement for each Class of Iris plant in the Iris dataset are:\n")
+    f.write('\n   \n')
+    f.write(str(minbyclass))
+    f.write('\n   \n')
+    f.write('Summary of each variable of each class:   \n')
+    f.write(str(describebyclass))
+
+
+
+
+
+
+
+
 
 #2.Saves a histogram of each variable to png files, and
 iris['sepal_length'].hist(bins=30) #plot the histogram of sepal lenght
@@ -41,18 +139,18 @@ plt.title('Histogram of sepal lenght') #title of histogram
 plt.xlabel('Sepal lenght in cm') #x axis label
 plt.ylabel('Number of sample') #y axis label
 plt.savefig('images/iris_hist_sepallenght.png') #save plot
-#plt.show() #show the histogram
+plt.show() #show the histogram
 
 iris['sepal_width'].hist(bins=30) #plot the histogram of sepal_width
 plt.title('Histogram of sepal_width') #title of histogram
-plt.xlabel('sepal_width in cm') #x axis label
+plt.xlabel('Sepal width in cm') #x axis label
 plt.ylabel('Number of sample') #y axis label
 plt.savefig('images/iris_hist_sepal_width.png') #save plot
 plt.show() #show the histogram
 
 iris['petal_length'].hist(bins=30) #plot the histogram of petal_length
-plt.title('Histogram of petal_length') #title of histogram
-plt.xlabel('petal_length in cm') #x axis label
+plt.title('Histogram of petal length') #title of histogram
+plt.xlabel('Petal length in cm') #x axis label
 plt.ylabel('Number of sample') #y axis label
 plt.savefig('images/iris_hist_petal_length.png') #save plot
 plt.show() #show the histogram
@@ -60,7 +158,7 @@ plt.show() #show the histogram
 
 iris['petal_width'].hist(bins=30) #plot the histogram of petal_width
 plt.title('Histogram of petal_width') #title of histogram
-plt.xlabel('petal_width in cm') #x axis label
+plt.xlabel('Petal width in cm') #x axis label
 plt.ylabel('Number of sample') #y axis label
 plt.savefig('images/iris_hist_petal_width.png') #save plot
 plt.legend()
@@ -69,33 +167,9 @@ plt.show() #show the histogram
 
 iris.hist() #histogram plot for all variables
 plt.savefig('images/iris_main_hist.png')  # save plot
-#plt.show() #show plot
+plt.show() #show plot
 
-
-
-versicolor = iris[iris['class'] == "Iris-versicolor"]
-print(versicolor)
-setosa = iris[iris['class']== "Iris-setosa"]
-virginica = iris[iris['class'] == 'Iris-virginica']
-
-'''
-
-points = plt.scatter(versicolor['petal_length'], versicolor["petal_width"], label="Iris-versicolor", facecolor = 'green')
-plt.scatter(setosa['petal_length'], setosa["petal_width"], label="Iris-setosa", marker='x', facecolor='blue')
-plt.scatter(virginica['petal_length'], virginica["petal_width"], label='Iris-virginica', marker='s', facecolor='red')
-'''
-
-iris.plot(x='petal_length', y="petal_width", kind= 'scatter')
-
-plt.xlabel("Petal Length (cm)")
-plt.ylabel("Petal Width (cm)")
-plt.title("Iris Petal Sizes")
-plt.savefig('images/scatter_petal_lenghtvswidth.png')  # save plot
-plt.legend()
-#plt.show()
-
-
-## Scatter plot of each pair of variables
+########## Scatter plot of each pair of variables
 #Idea reference is Data Camp Course 4
 
 ax = iris[iris['class']== "Iris-setosa"].plot(x='petal_length', y="petal_width", kind= 'scatter', c='red')
@@ -107,16 +181,41 @@ plt.ylabel("Petal Width (cm)")
 plt.title("Iris Petal Sizes")
 plt.savefig('images/scatter_petal_lenghtvswidth_allclasses.png')  # save plot
 plt.legend(["Iris-setosa", "Iris-versicolor", 'Iris-virginica'])
-#plt.show() #show scatter plot
+plt.show() #show scatter plot
 
 # scatter plot for pair of parameters  'sepal_length','sepal_width'
-ax = iris[iris['class']== "Iris-setosa"].plot(x='sepal_length', y="petal_width", kind= 'scatter', c='red')
-iris[iris['class']== "Iris-versicolor"].plot(x='sepal_length', ax=ax, y="petal_width", kind= 'scatter', c= 'green')
-iris[iris['class']== "Iris-virginica"].plot(x='sepal_length', ax=ax, y="petal_width", kind= 'scatter', c='blue')
+ax = iris[iris['class']== "Iris-setosa"].plot(x='sepal_length', y="sepal_width", kind= 'scatter', c='red')
+iris[iris['class']== "Iris-versicolor"].plot(x='sepal_length', ax=ax, y="sepal_width", kind= 'scatter', c= 'green')
+iris[iris['class']== "Iris-virginica"].plot(x='sepal_length', ax=ax, y="sepal_width", kind= 'scatter', c='blue')
 
 plt.xlabel("Sepal Length (cm)")
 plt.ylabel("Sepal Width (cm)")
 plt.title("Iris Sepal Sizes")
 plt.savefig('images/scatter_sepal_lenghtvswidth_allclasses.png')  # save plot
+plt.legend(["Iris-setosa", "Iris-versicolor", 'Iris-virginica'])
+plt.show() #show scatter plot
+
+
+
+
+ax = iris[iris['class']== "Iris-setosa"].plot(x='sepal_length', y="petal_width", kind= 'scatter', c='red')
+iris[iris['class']== "Iris-versicolor"].plot(x='sepal_length', ax=ax, y="petal_width", kind= 'scatter', c= 'green')
+iris[iris['class']== "Iris-virginica"].plot(x='sepal_length', ax=ax, y="petal_width", kind= 'scatter', c='blue')
+
+plt.xlabel("Sepal Length (cm)")
+plt.ylabel("Petal Width (cm)")
+plt.title("Iris Sepal Length / Petal Width")
+plt.savefig('images/scatter_sepal_lenghtvspetalwidth_allclasses.png')  # save plot
+plt.legend(["Iris-setosa", "Iris-versicolor", 'Iris-virginica'])
+plt.show() #show scatter plot
+
+ax = iris[iris['class']== "Iris-setosa"].plot(x='petal_length', y="sepal_width", kind= 'scatter', c='red')
+iris[iris['class']== "Iris-versicolor"].plot(x='petal_length', ax=ax, y="sepal_width", kind= 'scatter', c= 'green')
+iris[iris['class']== "Iris-virginica"].plot(x='petal_length', ax=ax, y="sepal_width", kind= 'scatter', c='blue')
+
+plt.xlabel("Petal length (cm)")
+plt.ylabel("Sepal width (cm)")
+plt.title("Iris Petal Length / Sepal Width")
+plt.savefig('images/scatter_petal_lenghtvssepal_width_allclasses.png')  # save plot
 plt.legend(["Iris-setosa", "Iris-versicolor", 'Iris-virginica'])
 plt.show() #show scatter plot
